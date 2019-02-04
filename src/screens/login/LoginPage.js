087937d5a208e9fa7431436b9a0d8e20f254ae0a
Johnson Cheung <johnson_9669@yahoo.com.hk>
2019-02-04 17:06:52 +0800

    Created Login Page

---
+++ src/screens/login/LoginPage.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import './LoginPage.css'
+
+import MellowLogo from 'statics/images/mellow-logo-full.png'
+
+export default class LoginPage extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			username:'',
+			password:''
+		}
+	}
+
+	handleTextChange = (field, text) => {
+		this.setState({
+			[field]:text
+		})
+	}
+
+	render() {
+		return (
+			<div className="login-container">
+				<div className="login-logo">
+					<img
+						src={MellowLogo}
+						alt=""
+						height="100%"
+						width="100%"
+					/>
+				</div>
+				<form className="login-form">
+					<input 
+						type="text" 
+						name="username"
+						placeholder="Username"
+						value={this.state.username}
+						className="login-input"
+						onChange={() => this.handleTextChange("username")}
+					/>
+					<input 
+						type="password" 
+						name="password" 
+						placeholder="Password"
+						value={this.state.username}
+						className="login-input"
+						onChange={() => this.handleTextChange("password")}
+					/>
+				</form>
+				<button 
+					className="login-submit"
+					onClick={() => console.log("hi")}
+				>
+					Login
+				</button>
+			</div>
+		)
+	}
+}
