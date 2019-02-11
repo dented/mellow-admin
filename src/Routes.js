cb5dd9f4d7c0f433e6da0db64d4e30bcee125fb3
Johnson Cheung <johnson_9669@yahoo.com.hk>
2019-02-11 16:33:20 +0800

    initial commit for user details

--- src/Routes.js
+++ src/Routes.js
@@ -4,13 +4,15 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import LoginPage from 'screens/login/LoginPage'
 import DashboardHome from 'screens/dashboard/DashboardHome'
 import DashboardUsers from 'screens/dashboard/users/DashboardUsers'
+import DashboardUserDetails from 'screens/dashboard/users/DashboardUserDetails'
 
  const AppRoutes = () => (
 	<Router>
 		<Switch>
 			<Route path="/" exact component={LoginPage}/>
 			<Route path="/dashboard" component={DashboardHome}/>
-			<Route path="/users" component={DashboardUsers}/>
+			<Route path="/users" exact component={DashboardUsers}/>
+			<Route path="/users/:id" component={DashboardUserDetails}/>
 		</Switch>
 	</Router>
 )
---
+++ src/screens/dashboard/users/DashboardUserDetails.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import './DashboardUserDetails.css'
+
+import DashboardPage from '../components/DashboardPage'
+import DashboardTable from '../components/DashboardTable'
+
+const users = [
+	{
+		name:"Johnsonson",
+		role:"Child",
+		gender:"Male"
+	},
+	{
+		name:"Johnsonson",
+		role:"Child",
+		gender:"Male"
+	},
+	{
+		name:"Johnsonson",
+		role:"Child",
+		gender:"Male"
+	},
+	{
+		name:"Johnsonson",
+		role:"Child",
+		gender:"Male"
+	}
+]
+
+export default class DashboardUserDetails extends React.Component {
+	constructor(props){
+		super(props)
+		this.state={
+			user:null
+		}
+	}
+
+	componentDidMount() {
+		const id = this.props.match.params.id
+		if(id) {
+			const user = users[id]
+			this.setState({
+				user:user
+			})
+		}
+	}
+
+	render() {
+		console.log(this.props)
+		return (
+			<DashboardPage
+				title="User Details"
+				pathname={'/users'}
+			>
+			{
+				this.state.user != null
+				? (
+					<div>
+						<div className="userDetails">
+							<h3>Details</h3>
+							<table className="dashboard-details-table">
+								<tr>
+									<td className="dashboard-details-key">Name</td>
+									<td>{this.state.user.name}</td>
+								</tr>
+								<tr>
+									<td className="dashboard-details-key">Role</td>
+									<td>{this.state.user.role}</td>
+								</tr>
+								<tr>
+									<td className="dashboard-details-key">Gender</td>
+									<td>{this.state.user.gender}</td>
+								</tr>
+							</table>
+						</div>
+						<div className="userRelationshipsHeader">
+							<h3>Relationships</h3>
+						</div>
+						<DashboardTable
+							table_columns={[
+								"Name","Relation"
+							]}
+						/>
+					</div>
+				)
+				: null
+			}
+			</DashboardPage>
+		)
+	}
+}
